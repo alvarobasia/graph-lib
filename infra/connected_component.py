d1b379125f1d75a14d28b70feda761c31af293b1
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-07-07 09:06:09 -0300

    add connect component deep search in matrix

--- infra/connected_component.py
+++ infra/connected_component.py
@@ -1,15 +1,17 @@
 from typing import List
 from domain.Graph import Graph
+from utils.representation_type import Representation
+from utils.search_type import Search_type
 
 
 class Connected_Component:
     result: List[int]
     graph: Graph
     tag: int
-    representation: bool
-    search: bool
+    representation: Representation
+    search: Search_type
 
-    def __init__(self, graph: Graph, representation: bool, search: bool):
+    def __init__(self, graph: Graph, representation: Representation, search: Search_type):
         self.tag = 0
         self.graph = graph
         self.result = [0 for i in range(graph.nodes_number + 1)]
@@ -17,14 +19,14 @@ class Connected_Component:
         self.search = search
 
     def get_info(self):
-        if self.representation:
-            if self.search:
+        if self.representation == Representation.ADJACENCY_LIST:
+            if self.search == Search_type.DEEP_SEARCH:
                 return self.__get_connected_component_list_deep()
             else:
                 self.__get_connected_component_list_wide()
         else:
             if self.search:
-                self.__get_connected_component_matrix_deep()
+                return self.__get_connected_component_matrix_deep()
             else:
                 self.__get_connected_component_matrix_wide()
 
@@ -37,7 +39,12 @@ class Connected_Component:
         return self.result
 
     def __get_connected_component_matrix_deep(self):
-        pass
+        graph = self.graph.get_matrix_representation()
+        for u in range(len(graph)):
+            if self.result[u] == 0:
+                self.tag = self.tag + 1
+                self.__deep_search_matrix_connect(u, graph)
+        return self.result
 
     def __get_connected_component_list_wide(self):
         pass
@@ -50,3 +57,9 @@ class Connected_Component:
         for v in graph[el]:
             if self.result[v[0]] == 0:
                 self.__deep_search_list_connect(v[0], graph)
+
+    def __deep_search_matrix_connect(self, el, graph):
+        self.result[el] = self.tag
+        for i, v in enumerate(graph[el]):
+            if v != 0 and self.result[i] == 0:
+                self.__deep_search_matrix_connect(i, graph)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from domain.Graph import Graph
 from infra.graph_info import Graph_info
 from infra.graph_search import Graph_Search
 from infra.connected_component import Connected_Component
+from utils.representation_type import Representation
+from utils.search_type import Search_type
 s = []
 with open("teste.txt", "r") as fd:
     for line in fd:
@@ -12,5 +14,6 @@ result = get_correct_values(s)
 
 graph = Graph(result['nodes'], result['edges'], result['values'])
 
-g = Connected_Component(graph, True, True)
+g = Connected_Component(
+    graph, Representation.ADJACENCY_MATRIX, Search_type.DEEP_SEARCH)
 print(g.get_info())
