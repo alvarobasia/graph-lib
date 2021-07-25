5c6664b83a6e7aeaea284f7e721e16221a2a4b20
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-07-25 17:55:45 -0300

    remove number

--- domain/Graph.py
+++ domain/Graph.py
@@ -8,29 +8,26 @@ class Graph:
     nodes_number: int
     edges_number: int
     values: List[List[int]]
-    nodes_number: int
 
     def __init__(self, nodes_number: int, edges_number: int, values: List[List[int]]) -> None:
         self.edges_number = edges_number
         self.nodes_number = nodes_number
         self.values = values
-        self.nodes_number = np.max([x[0:2] for x in values])
 
     def get_matrix_representation(self) -> List[List[int]]:
-        list = [[]for x in range(self.nodes_number + 1)]
-        for v in range(len(list)):
-            list[v] = [0 for x in range(self.nodes_number + 1)]
-        # f = csc_matrix((self.nodes_number + 1, self.nodes_number + 1),
-        #                dtype=int8).toarray()
+        # list = [[]for x in range(self.nodes_number + 1)]
+        # for v in range(len(list)):
+        #     list[v] = [0 for x in range(self.nodes_number + 1)]
+        f = csc_matrix((self.nodes_number, self.nodes_number),
+                       dtype=int8).toarray()
         for v in self.values:
-            list[v[0]][v[1]] = v[2]
-            list[v[1]][v[0]] = v[2]
-        return list
+            f[v[0]][v[1]] = v[2]
+            f[v[1]][v[0]] = v[2]
+        return f
 
     def get_list_representation(self) -> List[Tuple[int]]:
-        list = [[] for x in range(self.nodes_number + 1)]
+        list = [[] for x in range(self.nodes_number)]
         for v in self.values:
             list[v[0]].append((v[1], v[2]))
             list[v[1]].append((v[0], v[2]))
-        # print(list)
         return list
