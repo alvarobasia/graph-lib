b66b8f7a4c1ca3962ab1dbb489fa74b7929fcd16
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-07-26 14:29:37 -0300

    save infos in file

--- interface/output_graph_info.py
+++ interface/output_graph_info.py
@@ -1,10 +1,11 @@
-def print_graph_info(info):
-    print(
-        f"Grau máximo: {info['max_degree']['n']} \t Vértice: {info['max_degree']['v']}")
-    print(
-        f"Grau mínimo: {info['min_degree']['n']} \t Vértice: {info['min_degree']['v']}")
-    print(f"Grau médio: {info['avg']}")
-    print("Frequência relativa:")
-    for i, v in enumerate(info['freq']):
-        if v > 0:
-            print(f"Grau {i}: {v}")
+def print_graph_info(name, info):
+    with open(name, "w") as fd:
+        fd.write(
+            f"Grau máximo: {info['max_degree']['n']} \t Vértice: {info['max_degree']['v']}\n")
+        fd.write(
+            f"Grau mínimo: {info['min_degree']['n']} \t Vértice: {info['min_degree']['v']}\n")
+        fd.write(f"Grau médio: {info['avg']}\n")
+        fd.write("Frequência relativa:\n")
+        for i, v in enumerate(info['freq']):
+            if v > 0:
+                fd.write(f"Grau {i}: {v}\n")
